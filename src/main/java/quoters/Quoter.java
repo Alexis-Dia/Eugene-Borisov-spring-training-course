8618e005579897839a93c2655ca62210c5f919b5
Alexis-Dk <alexeydruzik@inbox.ru>
2020-03-21 16:09:54 +0300

    Added part1.

---
+++ src/main/java/quoters/Quoter.java
@@ -0,0 +1,8 @@
+package quoters;
+
+/**
+ * @author adruzik on 3/21/2020
+ */
+public interface Quoter {
+    void sayQuote();
+}
---
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -0,0 +1,18 @@
+package quoters;
+
+/**
+ * @author adruzik on 3/21/2020
+ */
+public class TerminatorQuoter implements Quoter {
+
+    private String message;
+
+    @Override
+    public void sayQuote() {
+        System.out.println("message = " + message);
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
