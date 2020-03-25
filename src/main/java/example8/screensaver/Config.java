dc62d5893fab01dbdf5ad248cc4192646f198372
Alexis-Dk <alexeydruzik@inbox.ru>
2020-03-25 13:17:16 +0300

    Added example8.

---
+++ src/main/java/example8/screensaver/Config.java
@@ -0,0 +1,28 @@
+package example8.screensaver;
+
+import org.springframework.context.annotation.*;
+
+import java.awt.*;
+import java.util.Random;
+
+/**
+ * @author Alexey Druzik on 3/25/2020
+ */
+@Configuration
+@ComponentScan(basePackages = "example8.screensaver")
+public class Config {
+    @Bean
+    @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public Color color() {
+        Random random = new Random();
+        return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        while (true) {
+            context.getBean(ColorFrame.class).showOnRandomPlace();
+            Thread.sleep(50);
+        }
+    }
+}
